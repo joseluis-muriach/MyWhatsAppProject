f97f5f35e724f17cda4b10416e422f96abc0a047
Jose Luis Muriach <alumur497@ieselcaminas.org>
2023-11-15 17:00:11 +0100

    Added pause_play and emoji

--- app/src/main/java/com/example/mywhatsappproject/Tabs.kt
+++ app/src/main/java/com/example/mywhatsappproject/Tabs.kt
@@ -1,15 +1,31 @@
 package com.example.mywhatsappproject
 
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import kotlinx.coroutines.launch
 
@@ -18,7 +34,7 @@ import kotlinx.coroutines.launch
 fun myTabs() {
     //Esto es para situarte en las pantallas (chat, novedades y llamadas) tenemos que hacerlo así
     //por que al añadir el corazon rotante cambia las librerias y por ende la versión
-    var pagerState = rememberPagerState(initialPage = 0, initialPageOffsetFraction = 0f){3}
+    var pagerState = rememberPagerState(initialPage = 0, initialPageOffsetFraction = 0f) { 3 }
     //Permite navegar por el resto de pantallas
     var scope = rememberCoroutineScope()
 
@@ -52,27 +68,58 @@ fun myTabs() {
         }
         //Esto te permite realizar la navegabilidad lateral
         HorizontalPager(
-            state = pagerState) { page ->
+            state = pagerState
+        ) { page ->
             when (page) {
                 0 -> chats()
                 1 -> Novedades()
-                2 ->Llamadas()
+                2 -> Llamadas()
             }
         }
-   }
+    }
 }
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
 fun Llamadas() {
-    Text(
-        text = "Llamadas"
+    //Aquí llamamos a "ic_smile" que es donde lo hemos enlazado con la rotación
+    val image =
+        AnimatedImageVector.animatedVectorResource(
+            R.drawable.cara_smile
+        )
+
+    var atEnd by remember { mutableStateOf(false) }
+
+    Image(
+        alignment = Alignment.Center,
+        painter = rememberAnimatedVectorPainter(image, atEnd),
+        contentDescription = "VectorDrawable",
+        modifier = Modifier.clickable {
+            atEnd = !atEnd
+        },
+        contentScale = ContentScale.Crop
     )
 }
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
 fun Novedades() {
-    Text(
-        text = "Novedades"
+    //Aquí llamamos a "Pause play" que es donde lo hemos enlazado con la rotación
+    val image =
+        AnimatedImageVector.animatedVectorResource(
+            R.drawable.pause_play
+        )
+
+    var atEnd by remember { mutableStateOf(false) }
+
+    Image(
+        alignment = Alignment.Center,
+        painter = rememberAnimatedVectorPainter(image, atEnd),
+        contentDescription = "VectorDrawable",
+        modifier = Modifier.clickable {
+            atEnd = !atEnd
+        },
+        contentScale = ContentScale.Crop
     )
 }
 
